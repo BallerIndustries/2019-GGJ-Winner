bc5f67cbffa775cd2f681e597b5eb949b7333f8c
anguruso <baller.industries@hotmail.com>
2019-01-26 22:12:58 +0800

    Velocity fun

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -68,18 +68,19 @@ export default class Game extends Phaser.Scene {
 
         // const r = 5;
         const radians = degreesToRadians(this.playerSprite.angle - 90);
-        const x = (6 * Math.sin(radians));
-        const y = (6 * Math.cos(radians));
+        const x = (200 * Math.sin(radians));
+        const y = (200 * Math.cos(radians));
 
         if (this.cursors.up.isDown) {
-            this.playerContainer.x -= x;
-            this.playerContainer.y += y;
+            this.playerContainer.body.setVelocity(-x, y);
             hasMoved = true;
         }
         else if (this.cursors.down.isDown) {
-            this.playerContainer.x += x;
-            this.playerContainer.y -= y;
+            this.playerContainer.body.setVelocity(x, -y);
             hasMoved = true;
+        }
+        else {
+            this.playerContainer.body.setVelocity(0, 0);
         }
 
         if (hasMoved) {
@@ -104,6 +105,12 @@ export default class Game extends Phaser.Scene {
 
         const playerNameGameObject = this.add.text(-20, -40, name, {fontSize: '14px', fill: '#000000'});
         playerContainer.add([playerSprite, playerNameGameObject]);
+
+
+        // Add a collider
+        playerContainer.setSize(playerSprite.width, playerSprite.height);
+        this.physics.world.enable(playerContainer);
+
         return {playerContainer, playerSprite};
     }
 
